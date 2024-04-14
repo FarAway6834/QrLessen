b89086d3b0848066f72ba555b69f55d75a73abbc
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2024-04-14 11:38:24 +0900

    Update conv2mylib.py

--- conv2mylib.py
+++ conv2mylib.py
@@ -3,4 +3,4 @@ def gen(func):
 	return lambda obj, f, scale : getattr(obj, func)(f, scale=scale)
 def terminal(quiet_zone):
 	return qrcode.terminal(quiet_zone=quiet_zone)
-svg, eps, png = gen('svg'), gen('eps'), png('png')
+svg, eps, png = gen('svg'), gen('eps'), gen('png')
