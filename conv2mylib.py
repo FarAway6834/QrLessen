5079cf8f32573c33e7ce8c4ce2243863fca5748b
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2024-04-14 11:37:16 +0900

    Delete conv2mylib.py

--- conv2mylib.py
+++
@@ -1,6 +0,0 @@
-from pyqrcode import create as makeQRObj
-def gen(func):
-	return lambda obj, f, scale : getattr(obj, func)(f, scale=scale)
-def terminal(quiet_zone):
-	return qrcode.terminal(quiet_zone=quiet_zone)
-svg, eps = gen('svg'), gen('eps'), png('png')
